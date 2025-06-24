8bf231f70fbd4152b47b62dac53e2c5d2d18bd28
LAKSHIYA SRI  K M <231501082@rajalakshmi.edu.in>
2025-06-24 19:16:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -75,5 +75,9 @@ def api_predict():
     except Exception as e:
         return jsonify({'error': str(e)})
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
